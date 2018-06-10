164b026dff8fe59f343facf02681eb86657b883c
Austin Erb <austin.erb8@gmail.com>
2018-06-10 15:00:51 -0400

    Finished Reddit navbar

--- js/reddit.js
+++ js/reddit.js
@@ -4,7 +4,17 @@ function redditJS() {
   var menu = document.getElementsByClassName("reddit-menu")[0];
   var menuBtn = document.getElementsByClassName("reddit-menu-btn")[0];
   var menuExitBtn = document.getElementsByClassName("reddit-menu-exit-btn")[0];
+  var favoritesHeader = document.getElementsByClassName("reddit-favorites-header")[0];
+  var favoritesHeaderUp = document.getElementsByClassName("reddit-favorites-header-up")[0];
+  var favoritesHeaderDown = document.getElementsByClassName("reddit-favorites-header-down")[0];
+  var favorites = document.getElementsByClassName("reddit-favorites")[0];
+  var subscriptionsHeader = document.getElementsByClassName("reddit-subscriptions-header")[0];
+  var subscriptionsHeaderUp = document.getElementsByClassName("reddit-subscriptions-header-up")[0];
+  var subscriptionsHeaderDown = document.getElementsByClassName("reddit-subscriptions-header-down")[0];
+  var subscriptions = document.getElementsByClassName("reddit-subscriptions")[0];
   var userDropdownOpen = false;
+  var favoritesOpen = true;
+  var subscriptionsOpen = true;
 
   userBtn.addEventListener("click", function(){
     if (!userDropdownOpen) {
@@ -28,6 +38,38 @@ function redditJS() {
     menu.style.display = "none";
 
     menuBtn.style.display = "inline-block";
+  });
+
+  favoritesHeader.addEventListener("click", function(){
+    if (favoritesOpen) {
+      favoritesOpen = false;
+
+      favoritesHeaderUp.style.display = "inline-block";
+      favoritesHeaderDown.style.display = "none";
+      favorites.style.display = "none";
+    } else {
+      favoritesOpen = true;
+
+      favoritesHeaderUp.style.display = "none";
+      favoritesHeaderDown.style.display = "inline-block";
+      favorites.style.display = "block";
+    }
+  });
+
+  subscriptionsHeader.addEventListener("click", function(){
+    if (subscriptionsOpen) {
+      subscriptionsOpen = false;
+
+      subscriptionsHeaderUp.style.display = "inline-block";
+      subscriptionsHeaderDown.style.display = "none";
+      subscriptions.style.display = "none";
+    } else {
+      subscriptionsOpen = true;
+
+      subscriptionsHeaderUp.style.display = "none";
+      subscriptionsHeaderDown.style.display = "inline-block";
+      subscriptions.style.display = "block";
+    }
   });
 };
 
